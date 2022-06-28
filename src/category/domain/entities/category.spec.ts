25dca547da74eee317762aab82c06219f87f3d91
joao pedro <joaopedro.lacerda33@gmail.com>
2022-06-28 19:27:30 -0300

    refactor: change test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,55 @@
 import { Category } from "./category";
-
+import { omit } from "lodash";
 describe("Category Unit test", (): void => {
   test("constructor of category", () => {
-    const category = new Category('movie');
-    expect(category.name).toBe("movie1")
+    let category = new Category({ name: 'movie' });
+
+    let props = omit(category.props, "created_at")
+
+    expect(props).toStrictEqual({
+      name: 'movie',
+      description: null,
+      is_active: true,
+    })
+    expect(category.props.created_at).toBeInstanceOf(Date)
+
+    category = new Category({
+      name: "movie",
+      description: "some description",
+      is_active: false
+    })
+    let created_at = new Date();
+
+    expect(category.props).toStrictEqual({
+      name: 'movie',
+      description: 'some description',
+      is_active: false,
+      created_at
+    })
+    category = new Category({
+      name: "movie",
+      description: "other description",
+    })
+    expect(category.props).toMatchObject({
+      name: 'movie',
+      description: 'other description',
+    })
+    category = new Category({
+      name: "movie",
+      is_active: true,
+    })
+    expect(category.props).toMatchObject({
+      name: 'movie',
+      is_active: true,
+    })
+    created_at = new Date();
+    category = new Category({
+      name: "movie",
+      created_at,
+    })
+    expect(category.props).toMatchObject({
+      name: 'movie',
+      created_at,
+    })
   })
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,36 @@
+export type CategoryProperties = {
+  name: string, 
+  description?: string, 
+  is_active?: boolean, 
+  created_at?: Date
+}
 export class Category {
-  constructor(public  name: string){
-
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
   }
+  get name(): string {
+    return this.props.name;
+  }
+  get description(): string | undefined {
+    return this.props.description;
+  }
+
+  private set description(value: string) {
+    this.props.description = value ?? null
+  }
+
+  get is_active(): boolean | undefined {
+    return this.props.is_active;
+  }
+  
+  private set is_active(value: boolean) {
+    this.props.is_active = this.props.is_active ?? true
+  }
+
+  get created_at(): Date | undefined {
+    return this.props.created_at;
+  }
+
 }
