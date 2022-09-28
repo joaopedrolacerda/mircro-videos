3ca4fc0a375d714efc81f293eb6fa69446f2aeee
joao pedro <joaopedro.lacerda33@gmail.com>
2022-09-28 07:48:27 -0300

    fix: correction unique-entity-id

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,16 +1,20 @@
-import InvalidUuidError from '@seedwork/errors/invalid-uuid.error';
-import {v4 as uuidv4, validate as uuidValidate} from 'uuid';
-import ValueObject from './value-object';
-export default class UniqueEntityId extends ValueObject<String> {
-  constructor(readonly id?: string){
-    super( id || uuidv4());
-    this.validate()
-  }
-  private validate(){
-      const isValid = uuidValidate(this._value);
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
+import InvalidUuidError from "../../errors/invalid-uuid.error";
+import ValueObject from "./value-object";
 
-      if(!isValid){
-        throw new InvalidUuidError();
-      }
+export class UniqueEntityId extends ValueObject<string> {
+  constructor(readonly id?: string) {
+    super(id || uuidv4());
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.value);
+    if (!isValid) {
+      throw new InvalidUuidError();
+    }
   }
 }
+
+export default UniqueEntityId;
+//Projeto é código e o código é projeto
