398ada13afbb55800698681c1ef37e6f8be57f4e
joao pedro <joaopedro.lacerda33@gmail.com>
2022-10-04 11:29:34 -0300

    feat: add tests

--- src/@seedwork/validators/validator-rules.spec.ts
+++ src/@seedwork/validators/validator-rules.spec.ts
@@ -1,6 +1,33 @@
 import ValidationError from "@seedwork/errors/validation-error";
 import ValidatorRules from "./validator-rules"
 
+
+type expectedValidationRule = {
+  value: any;
+  property: string;
+  rule?: keyof ValidatorRules;
+}
+type ExpectedRule = {
+  value: any;
+  property: string;
+  rule: keyof ValidatorRules;
+  error: ValidationError;
+  params?: any[]
+}
+function assertIsInvalid({value, property, rule, error, params = []}:ExpectedRule){
+  expect(() => {
+    const validator = ValidatorRules.values(value, property)
+    const method:any = validator[rule];
+    method.apply(validator, params)
+  }).toThrow(error)
+}
+function assertIsValid({value, property, rule, error, params = []}:ExpectedRule){
+  expect(() => {
+    const validator = ValidatorRules.values(value, property)
+    const method:any = validator[rule];
+    method.apply(validator, params)
+  }).not.toThrow(error)
+}
 describe('Validator Rules Unit Tests', () => {
   test('values method', () => {
     const validator = ValidatorRules.values("some value", "field");
@@ -12,22 +39,59 @@ describe('Validator Rules Unit Tests', () => {
   test('required validation rule', () => {
     // invalid cases
 
-    let arrange: {value: any, property: string, messageError: string}[] = [
-      {value:null, property: "field", messageError: 'the field is required'},
-      {value: undefined, property: "field", messageError: 'the field is required'},
-      {value:"", property: "field", messageError: 'the field is required'}
+    let arrange: expectedValidationRule[] = [
+      {value:null, property: "field"},
+      {value: undefined, property: "field"},
+      {value:"", property: "field"}
     ]
+    const error = new ValidationError("the field is required")
     arrange.forEach(item => {
-    expect( () => ValidatorRules.values(item.value, item.property).required()).toThrow(new ValidationError(item.messageError))
+      assertIsInvalid({value:item.value, property:item.property, rule: "required", error: error })
     })
     arrange = [
-      {value:"test", property: "field", messageError: 'the field is required'},
-      {value: 5, property: "field", messageError: 'the field is required'},
-      {value:0, property: "field", messageError: 'the field is required'},
-      {value:false, property: "field", messageError: 'the field is required'}
+      {value:"test", property: "field"},
+      {value: 5, property: "field"},
+      {value:0, property: "field"},
+      {value:false, property: "field"}
     ]
     arrange.forEach(item => {
-      expect( () => ValidatorRules.values(item.value, item.property).required()).not.toThrow(new ValidationError(item.messageError))
+      assertIsValid({value:item.value, property:item.property, rule: "required", error: error })
+      })
+  })
+
+  test("string validation rule", () => {
+    // invalid cases
+    const error = new ValidationError("the field must be a string")
+    let arrange: expectedValidationRule[] = [
+      {value:5, property: "field"},
+      {value: {}, property: "field"},
+      {value:false, property: "field"}
+    ]
+    arrange.forEach(item => {
+      assertIsInvalid({value:item.value, property:item.property, rule: "String", error: error})
+    })
+    arrange = [
+      {value:"test", property: "field"},
+    ]
+    arrange.forEach(item => {
+      assertIsValid({value:item.value, property:item.property, rule: "String", error: error})
+      })
+  })
+
+  test("maxLength validation rule", () => {
+    let arrange: expectedValidationRule[] = [
+      {value:"aaaaaa", property: "field"},
+    ]
+    const error = new ValidationError("the field must be less or equal than 5")
+
+    arrange.forEach(item => {
+      assertIsInvalid({value:item.value, property:item.property, rule: "maxLength", error: error, params: [5]})
+    })
+    arrange = [
+      {value:"aaaaaa", property: "field"},
+    ]
+    arrange.forEach(item => {
+      assertIsValid({value:item.value, property:item.property, rule: "maxLength", error: error,  params: [5]})
       })
   })
 })
